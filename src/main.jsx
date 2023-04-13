b52beb5f7f911c124ce52bc3c18975b76cb615e2
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-13 12:38:56 -0600

    creatind contacts

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,21 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
-import Root from './routes/root';
+import './index.css';
 import ErrorPage from './components/ErrorPage';
 import Contact from './routes/Contact';
+import Root, { 
+  loader as rootLoader,
+  action as rootAction
+} from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     errorElement: <ErrorPage></ErrorPage>,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
